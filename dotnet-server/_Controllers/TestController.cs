a70bc0d2d1ce641c283c3c5ac6efe75b2926da8c
Phongsavanh Mongkhonvilay <pmongkho12@gmail.com>
2026-03-12 11:03:23 -0500

    Fix TestController DI constructor ambiguity

--- dotnet-server/_Controllers/TestController.cs
+++ dotnet-server/_Controllers/TestController.cs
@@ -1,4 +1,6 @@
+using dotnet_server._Data;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace dotnet_server.Controllers;
 
@@ -6,12 +8,47 @@ namespace dotnet_server.Controllers;
 [Route("api/[controller]")]
 public class TestController : ControllerBase
 {
+    private readonly AppDbContext dbContext;
+
+    public TestController(AppDbContext dbContext)
+    {
+        this.dbContext = dbContext;
+    }
+
     [HttpGet]
     public IActionResult Get()
     {
         return Ok(new
         {
             message = "Backend is connected",
+            time = DateTime.UtcNow
+        });
+    }
+
+    [HttpGet("db")]
+    public async Task<IActionResult> CheckDatabaseConnection()
+    {
+        var canConnect = await dbContext.Database.CanConnectAsync();
+
+        if (!canConnect)
+        {
+            return StatusCode(StatusCodes.Status503ServiceUnavailable, new
+            {
+                connected = false,
+                message = "Database is not reachable",
+                time = DateTime.UtcNow
+            });
+        }
+
+        var timestamp = await dbContext.Database
+            .SqlQueryRaw<DateTime>("SELECT NOW()")
+            .SingleAsync();
+
+        return Ok(new
+        {
+            connected = true,
+            message = "Database connection is healthy",
+            dbTime = timestamp,
             time = DateTime.UtcNow
         });
     }
